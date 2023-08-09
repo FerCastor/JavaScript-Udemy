b88f5c9304b0a801e142bc1356b0ab3c4481e6f2
Fernanda Castor <102315480+FerCastor@users.noreply.github.com>
2023-08-09 08:22:51 -0300

    Create index.js

---
+++ aula028/index.js
@@ -0,0 +1,7 @@
+// const tresHoras = 60 * 60 * 3 * 1000 // 60 segundos * 60 minutos * 3 horas * 1000 milissegundos
+// const umDia = 60 * 60 * 24 * 1000
+// const data = new Date(0 + tresHoras);
+
+
+const data = new Date();
+console.log(data.toString());
