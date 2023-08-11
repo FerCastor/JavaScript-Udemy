5bc5389c85f3c0fdb33bfd4d2a4e1a48c8576f6c
Fernanda Castor <102315480+FerCastor@users.noreply.github.com>
2023-08-11 08:57:08 -0300

    retorno de funções

--- aula051/index.js
+++ aula051/index.js
@@ -1,7 +1,59 @@
 // return -> retorna um valor e termina a função
+// Quando há uma função dentro da outra e uma retorna a outra, é um retorno da função em si, sem a execução da mesma
 
-function soma(a, b) {
-    return a + b;
+function criaMultiplicador(multiplicador) {
+    return function (n) {
+        return n * multiplicador
+    };
 }
 
-console.log(soma(2, 5));
+const duplica = criaMultiplicador(2)
+const triplica = criaMultiplicador(3)
+const quadriplica = criaMultiplicador(4)
+const resultado1 = duplica(2)
+const resultado2 = triplica(2)
+const resultado3 = quadriplica(2)
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+
+
+
+/* function falaFrase(comeco) {
+    function falaResto(resto) {
+        return comeco + ' ' + resto;
+    }
+    return falaResto;
+}
+
+const fala = falaFrase('Olá');
+const resto = fala("Mundo");
+console.log(fala);
+console.log(resto); */
+
+
+/* function criaPessoa(nome, sobrenome) {
+    return {nome, sobrenome};
+}
+
+const pessoa1 = criaPessoa('Fer', 'Castor');
+console.log(pessoa1)
+console.log(pessoa1.nome)
+console.log(pessoa1.sobrenome)
+
+const pessoa2 = {
+    nome: 'Robbie',
+    sobrenome: 'Jackson',
+}
+
+console.log(pessoa2)
+console.log(pessoa2.nome)
+console.log(pessoa2.sobrenome) */
+
+/* document.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'black';
+    const p = document.createElement('p')
+    p.innerHTML = 'Texto de exemplo';
+    p.style.color = 'yellow';
+    document.body.appendChild(p)
+}); */
