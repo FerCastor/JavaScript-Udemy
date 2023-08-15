57152bebc93afb7de2c4f0b61e2959abb6ea8c25
Fernanda Castor <102315480+FerCastor@users.noreply.github.com>
2023-08-15 08:38:54 -0300

    Update index.js

--- aula073/index.js
+++ aula073/index.js
@@ -1,9 +1,9 @@
 /*
-Object.values
-Object.entrie
-Object.assign(des, any)
-Object.getOwnPropertyDescriptor(o, 'prop)
-... (spread)
+Object.values -> retorna os valores
+Object.entrie -> retorna a chave e o valor
+Object.assign(des, any) -> serve para copiar um objeto (objeto origem -> objeto destino)
+Object.getOwnPropertyDescriptor(o, 'prop) -> retorna o descritor da propriedade
+... (spread) -> espalha as propriedades de um objeto em outro objeto
 
 // Já visto
 Object.keys (retorna as chaves)
@@ -12,8 +12,29 @@ Object.defineProperties (define várias propriedades)
 Object.defineProperty(define uma propriedade)
 */
 
+const produto = {nome: 'Produto', preco: 1.8};
+Object.defineProperty(produto, 'nome', {
+    writable: false,
+    configurable: false,
+    value: 'Mais um produto'
+})
+console.log(Object.getOwnPropertyDescriptor(produto, 'nome'));
+produto.nome = 'Outro produto'
+console.log(Object.keys(produto))
+console.log(Object.values(produto));
+console.log(Object.entries(produto));
 
+for(let entry of Object.entries(produto)) {
+    console.log(entry)
+}
 
+for(let valor of Object.entries(produto)) {
+    console.log(valor[0], valor[1])
+}
+
+for(let [chave, valor] of Object.entries(produto)) {
+    console.log(chave, valor)
+}
 
 // SPREAD
 /* const produto = {nome: 'Produto', preco: 1.8};
