ed60af38187a09c5f2bc6a193767a1da2cb832a1
Fernanda Castor <102315480+FerCastor@users.noreply.github.com>
2023-08-18 10:41:47 -0300

    testes

--- testes/tests.js
+++
@@ -1,9 +0,0 @@
-class Calculadora {
-    constructor(...numeros) {
-        const display = document.querySelector('.display');
-    }
-
-    display.addEventListener('click', e => {
-        const elemento = e.target;
-    });
-}
