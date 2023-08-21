1ac3212ebee16da387084affef03d1a459a93680
Fernanda Castor <102315480+FerCastor@users.noreply.github.com>
2023-08-21 15:21:48 -0300

    Gerador de Senha

---
+++ aula096-gerador-de-senhas/src/main.js
@@ -0,0 +1,22 @@
+import './assets/css/style.css';
+
+
+
+
+function getPassword() {
+    const qtdCaracteres = document.querySelector('#qtd-caracteres');
+    const addNumber = document.querySelector('#add-numeros');
+    const maiusculas = document.querySelector('#letra-upper');
+    const minusculas = document.querySelector('#letra-lower');
+    const especiais = document.querySelector('#simbolos');
+    const botao = document.querySelector('#btn-enviar');
+
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:{}[]";
+    var passwordLength = Number(qtdCaracteres.value);
+    var password = "";
+
+
+}
+
+console.log(Number(qtdCaracteres.value))
+
