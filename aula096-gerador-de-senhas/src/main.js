c3a13b85829616d4e7005cee57f0ff002515cddf
Fernanda Castor <102315480+FerCastor@users.noreply.github.com>
2023-08-23 11:03:58 -0300

    Gerador de senhas

--- aula096-gerador-de-senhas/src/main.js
+++ aula096-gerador-de-senhas/src/main.js
@@ -1,22 +1,23 @@
 import './assets/css/style.css';
 
-
-
+const qtdCaracteres = document.querySelector('#qtd-caracteres');
+const addNumber = document.querySelector('#add-numeros');
+const maiusculas = document.querySelector('#letra-upper');
+const minusculas = document.querySelector('#letra-lower');
+const especiais = document.querySelector('#simbolos');
+const botao = document.querySelector('.btn-enviar'); // Use a classe para selecionar o botão
 
 function getPassword() {
-    const qtdCaracteres = document.querySelector('#qtd-caracteres');
-    const addNumber = document.querySelector('#add-numeros');
-    const maiusculas = document.querySelector('#letra-upper');
-    const minusculas = document.querySelector('#letra-lower');
-    const especiais = document.querySelector('#simbolos');
-    const botao = document.querySelector('#btn-enviar');
+    let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:{}[]";
+    let passwordLength = Number(qtdCaracteres.value);
+    let password = "";
 
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:{}[]";
-    var passwordLength = Number(qtdCaracteres.value);
-    var password = "";
+    for (let i = 0; i < passwordLength; i++) {
+        let randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
 
-
+    console.log(password);
 }
 
-console.log(Number(qtdCaracteres.value))
-
+botao.addEventListener('click', getPassword);
