ceb867e1456fd11f0b2582cf424852d39d9959a9
Fernanda Castor <102315480+FerCastor@users.noreply.github.com>
2023-08-24 17:53:35 -0300

    Express Middlewares

---
+++ node/aula013/frontend/main.js
@@ -0,0 +1,4 @@
+import 'core-js/stable' // // Resolve os casos de incompatibilidade com o js na versão mais recente, adaptando a execução em navegadores mais antigos
+import 'regenerator-runtime/runtime';
+import './assets/css/style.css';
+
