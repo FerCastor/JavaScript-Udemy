98121885e3a1ef5fa76fa9f0be58b93bdc0ebee4
Fernanda Castor <102315480+FerCastor@users.noreply.github.com>
2023-08-25 17:24:13 -0300

    Criando pag de login/criar conta

--- projetoagenda/src/controllers/homeController.js
+++ projetoagenda/src/controllers/homeController.js
@@ -1,28 +1,4 @@
-exports.paginaInicial = (req, res) => {
-	res.render("index", {
-		titulo: "Título da Página",
-		numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-	});
+exports.index = (req, res) => {
+	res.render("index");
 	return;
 };
-
-exports.trataPost = (req, res) => {
-	res.send(req.body);
-	return;
-};
-
-/* 
-const HomeModel = require("../models/HomeModel");
-
-// Busca todos os objetos no banco e exibe no console
-HomeModel.find()
-	.then((dados) => console.log(dados))
-	.catch((e) => console.log(e)); */
-
-/* // Cria um objeto no banco de dados
-HomeModel.create({
-    titulo: 'Segundo item',
-    descricao: 'Mais uma descrição'
-    })
-.then(dados => console.log(dados))
-.catch(e => console.log(e)) */
